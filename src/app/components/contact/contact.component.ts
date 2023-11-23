6bc075fdc5171714453aed84eb8e35c9872db271
Limber <limbpuma_de@hotmail.com>
2023-11-24 00:41:13 +0100

    message error solved2

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -18,7 +18,7 @@ emailjs.init(EMAILJS_USER_ID);
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
-
+ showError: boolean = false;
   name!: string;
     email!: string;
     body!: string;
@@ -26,30 +26,46 @@ export class ContactComponent {
   emailSent: boolean = false;
 
 onSubmit(data: any) {
+  // Validar que todos los campos están llenos
+  if (!this.name || !this.email || !this.body) {
+    console.log('Por favor, completa todos los campos.');
+    return;  // No proceder si algún campo está vacío
+  }
+
+  // Expresión regular para validar el correo electrónico
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  if (!emailRegex.test(this.email)) {
+    console.log('Please enter a valid email.');
+    return;  // No proceder si el correo electrónico no es válido
+  }
+
+
+  // Preparar los parámetros de la plantilla de EmailJS
   const templateParams = {
-    from_name: data.Name,
-    from_email: data.EmailAddress,
+    from_name: this.name,  // Usar la propiedad del componente
+    from_email: this.email, // Usar la propiedad del componente
     to_name: "Your Name",  // Esto puede ser tu nombre o lo que desees
-    message: data.Body,
+    message: this.body,    // Usar la propiedad del componente
     to_email: "limbpuma_de@hotmail.com" // Dirección de correo electrónico a la que deseas enviar el mensaje
   };
 
-   emailjs.send('service_rz82fcl', 'template_m0s2z7l', templateParams)
-      .then((response) => {
-         console.log('Email sent successfully!', response.status, response.text);
-         this.emailSent = true; // Agrega esta línea
-         this.clearForm();  // Agrega esta línea
-         setTimeout(() => this.emailSent = false, 3500); // Agrega esta línea
-      }, (error) => {
-         console.log('Failed to send the email.', error);
-      });
-  }
+  // Lógica para enviar el correo electrónico
+  emailjs.send('service_rz82fcl', 'template_m0s2z7l', templateParams)
+    .then((response) => {
+      console.log('Email sent successfully!', response.status, response.text);
+      this.emailSent = true;
+      this.clearForm();
+      setTimeout(() => this.emailSent = false, 3500);
+    }, (error) => {
+      console.log('Failed to send the email.', error);
+    });
+}
 
-  clearForm() {  // Agrega esta función
-    this.name = '';
-    this.email = '';
-    this.body = '';
-  }
+clearForm() {
+  this.name = '';
+  this.email = '';
+  this.body = '';
+}
 
    githubLink = PERSONAL_LINKS.github;
   linkedinLink = PERSONAL_LINKS.linkedin;
@@ -69,5 +85,7 @@ onSubmit(data: any) {
       emailjs.init(this.EMAILJS_USER_ID);
     });
   }
+
+  
 
 }
